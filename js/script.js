77465dbaa458db3908ea378e59eb6dbafc8f9807
goldipl <mgodlewski92@gmail.com>
2021-09-26 10:41:56 +0200

    handle closing mobile menu after choosing li nav

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const burger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobileMenu");
+const mobileMenuA = document.querySelector(".mobileMenu_li");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
+});
+
+mobileMenuA.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    burger.classList.remove("active");
 });
