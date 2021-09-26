e53adae8277552473a895cb57f5a41577ac3c893
goldipl <mgodlewski92@gmail.com>
2021-09-26 18:58:16 +0200

    handle news background color using JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobileMenu");
 const mobileMenuA = document.querySelector(".mobileMenu_li");
+const html = document.querySelector("html");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
@@ -11,3 +12,7 @@ mobileMenuA.addEventListener("click", () => {
     mobileMenu.classList.remove("active");
     burger.classList.remove("active");
 });
+
+if (document.body.classList.contains("news")) {
+    html.classList.add("greyHTML");
+};
