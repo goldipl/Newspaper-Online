efe8a6be72ea995ea7fd382eb1bc4bb5c1062050
goldipl <mgodlewski92@gmail.com>
2021-10-03 21:18:29 +0200

    fix js problem

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const burger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobileMenu");
-const mobileMenuA = document.querySelector(".mobileMenu_li li");
+const mobileMenuA = document.querySelector(".mobileMenu_li");
 const html = document.querySelector("html");
 const bodyNews = document.querySelector("body.news");
 const body = document.querySelector("body");
@@ -9,7 +9,7 @@ burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
     body.classList.toggle("bodyBlocked");
-    if (burger.classList.contains("active")) {
+    if ((burger.classList.contains("active")) && (body.classList.contains("news"))) {
         bodyNews.classList.add("fullWidth");
         burger.classList.add("fullWidthBurger");
     } else {
@@ -19,6 +19,7 @@ burger.addEventListener("click", () => {
 });
 
 mobileMenuA.addEventListener("click", () => {
+    body.classList.remove("bodyBlocked");
     mobileMenu.classList.remove("active");
     burger.classList.remove("active");
 });
