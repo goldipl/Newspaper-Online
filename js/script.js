b6604dcfbe39a82fc383882c429be9f2eaf448a6
goldipl <mgodlewski92@gmail.com>
2021-10-03 13:27:49 +0200

    handle bodyBlocked and hamburger localization

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@ const burger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobileMenu");
 const mobileMenuA = document.querySelector(".mobileMenu_li");
 const html = document.querySelector("html");
+const bodyNews = document.querySelector("body.news");
+const body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
+    body.classList.toggle("bodyBlocked");
+    if (burger.classList.contains("active")) {
+        bodyNews.classList.add("fullWidth");
+        burger.classList.add("fullWidthBurger");
+    } else {
+        bodyNews.classList.remove("fullWidth");
+        burger.classList.remove("fullWidthBurger");
+    };
 });
 
 mobileMenuA.addEventListener("click", () => {
