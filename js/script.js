7bf4ac1a41739e6eac45df8ffb3f1d2f78508ae6
goldipl <mgodlewski92@gmail.com>
2021-10-03 21:22:35 +0200

    clean js code

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ const mobileMenu = document.querySelector(".mobileMenu");
 const mobileMenuA = document.querySelector(".mobileMenu_li");
 const html = document.querySelector("html");
 const bodyNews = document.querySelector("body.news");
-const body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
-    body.classList.toggle("bodyBlocked");
-    if ((burger.classList.contains("active")) && (body.classList.contains("news"))) {
+    document.body.classList.toggle("bodyBlocked");
+    if ((burger.classList.contains("active")) && (document.body.classList.contains("news"))) {
         bodyNews.classList.add("fullWidth");
         burger.classList.add("fullWidthBurger");
     } else {
@@ -19,7 +18,7 @@ burger.addEventListener("click", () => {
 });
 
 mobileMenuA.addEventListener("click", () => {
-    body.classList.remove("bodyBlocked");
+    document.body.classList.remove("bodyBlocked");
     mobileMenu.classList.remove("active");
     burger.classList.remove("active");
 });
